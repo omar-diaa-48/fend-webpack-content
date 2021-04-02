283ae58a3acb8fe8b092f9d43c6fbb974f0535d0
Omar Diaaeldine Elwakeel <67291253+omar-diaa-48@users.noreply.github.com>
2021-04-02 21:12:24 +0200

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,14 @@ module.exports = {
                 test:'/\.js$/',
                 exclude: /node_modules/,
                 loader:'babel-loader'
+            },
+            {
+                test:'/\.(png|jpe?g)|gif)$/i',
+                use:[
+                    {
+                        loader:'file-loader'
+                    }
+                ]
             }
         ]
     }
